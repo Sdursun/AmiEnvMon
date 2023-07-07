c492b295e4af6f613ea6628e7212660d93affe4b
Marie Katrine Ekeberg <mke@themkat.net>
2023-07-07 13:49:12 +0200

    Fixed a segfault when json data is cutoff

--- src/envmondata.c
+++ src/envmondata.c
@@ -26,19 +26,29 @@ struct EnvironmentMonitorData *EnvMon_FromJsonString(const char* json_string) {
                                         strnlen(json_string, MAX_JSON_STRING_LENGTH),
                                         json_tokens,
                                         MAX_JSON_TOKENS);
-
+  
   size_t i;
+  size_t num_read = 0;
   for(i = 0; i < num_parsed_tokens; i++) {
     size_t string_length = json_tokens[i].end - json_tokens[i].start;
     
     if(0 == strncmp(json_string + json_tokens[i].start, "temperature", string_length)) {
+      num_read++;
       data->temperature = atof(json_string + json_tokens[i + 1].start);
     }
     else if(0 == strncmp(json_string + json_tokens[i].start, "humidity", string_length)) {
+      num_read++;
       data->humidity = atof(json_string + json_tokens[i + 1].start);
     }
     else if(0 == strncmp(json_string + json_tokens[i].start, "eCO2", string_length)) {
+      num_read++;
       data->eCO2 = atoi(json_string + json_tokens[i + 1].start);
+    }
+
+    // stupid fix for invalid data to avoid segfaulting
+    // far from perfect. Need to find a better more longterm solution
+    if(3 <= num_read) {
+      break;
     }
   }
   
--- tests/envmondata_test.c
+++ tests/envmondata_test.c
@@ -27,9 +27,21 @@ TEST(envmondata, JsonParsedSuccesfully) {
   TEST_ASSERT_EQUAL_INT(400, data->eCO2);
 }
 
+TEST(envmondata, JsonDataCuttoff) {
+  // this seems to happen sometimes...
+  // We should support it without segfaulting
+  data = EnvMon_FromJsonString("{\"eCO2\": 400, \"humidity\": 32.4188, \"temperature\": 23.228");
+  
+  TEST_ASSERT_NOT_NULL(data);
+  TEST_ASSERT_EQUAL_FLOAT(23.228, data->temperature);
+  TEST_ASSERT_EQUAL_FLOAT(32.4188, data->humidity);
+  TEST_ASSERT_EQUAL_INT(400, data->eCO2);
+}
+
 // TODO: test case for some sort of invalid data? is that even possible?
 
 TEST_GROUP_RUNNER(envmondata) {
   RUN_TEST_CASE(envmondata, EmptyInputShouldReturnNull);
   RUN_TEST_CASE(envmondata, JsonParsedSuccesfully);
+  RUN_TEST_CASE(envmondata, JsonDataCuttoff);
 }
