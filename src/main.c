aaf69479800c59100f801785af88793cd5a47920
Marie Katrine Ekeberg <mke@themkat.net>
2023-07-07 14:21:34 +0200

    Invoke GUI in main

--- src/main.c
+++ src/main.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "envmondata.h"
 #include "httpget.h"
+#include "httpget_responseparse.h"
 #include "gui.h"
 
 int main() {
-  //printf("AmiEnvMon\n");
-  //httpget();
-  draw();
+  char* http_response = httpget();
+  char* json_body = getJsonBodyFromHttpResponse(http_response);  
+  struct EnvironmentMonitorData* envmondata = EnvMon_FromJsonString(json_body);
+
+  printf("json string: %s\n", json_body);
   
+  free(http_response);
+  free(json_body);
+  
+  GUI_SetEnvMonData(envmondata);
+  GUI_Draw();
+
+  EnvMon_Destroy(envmondata);
   return 0;
 }
