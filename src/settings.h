30a9fd0eabd4f3e477e78f06cafe01cc5d44b043
Marie Katrine Ekeberg <mke@themkat.net>
2023-08-25 16:36:19 +0200

    WIP. Draft for a simple settings system to be able to change IP etc. #4

---
+++ src/settings.h
@@ -0,0 +1,26 @@
+#ifndef SETTINGS_H
+#define SETTINGS_H
+
+// basic file for settings loading, settings struct, writing and reading the binary format
+
+enum TextBoxAlignment {
+  LEFT, RIGHT, CENTER
+};
+
+struct ApplicationSettings {
+  char* ip;
+  enum TextBoxAlignment number_boxes_alignment;
+};
+
+// Read application settings from a file and return a struct representing the data.
+// If the file could not be read, is not correctly formatted etc., NULL is returned.
+struct ApplicationSettings *Settings_ReadFromFile(const char* filename);
+
+// TODO: should we return a status or similar here? or just assume that writing
+// will not fail
+void Settings_WriteSettingsToFile(struct ApplicationSettings* settings, const char* filename);
+
+// Cleans up memory for the application settings struct.
+void Settings_Destroy(struct ApplicationSettings* settings);
+
+#endif
